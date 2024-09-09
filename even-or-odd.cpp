d5de242e6580ff13ed71f91177c39f4926277c83
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-09-09 22:25:43 +0530

    Add files via upload

---
+++ even-or-odd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,num;
+    cout<<"Enter your number : ";
+    cin>>num;
+
+    if(num%2==0)
+    {
+        cout<<num<<" Number is even ";
+    }
+    else
+    {
+        cout<<num<<" is odd number ";
+    }
+}
