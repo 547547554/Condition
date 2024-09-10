8d77d134f2637e778d51bf0a3c03172e2244c59a
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-09-10 21:26:03 +0530

    Add files via upload

---
+++ if-else.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int packege;
+    cin>>packege;
+
+    if(packege>10)
+    {
+        cout<<"accepted";
+    }
+    else
+    {
+        cout<<"Rejected";
+    }
+}
