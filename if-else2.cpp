e43880c0ab60539bf2b1c79c80e713e814593656
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-09-11 22:19:20 +0530

    Add files via upload

---
+++ if-else2.cpp
@@ -0,0 +1,18 @@
+//pass or fail marks
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int marks;
+    cin>>marks;
+
+    if(marks>33)
+    {
+        cout<<"Pass";
+    }
+    else
+    {
+        cout<<"Fail";
+    }
+}
