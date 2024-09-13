6524ef3713a30884570f26fda8c66a62d183fcb4
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-09-13 22:54:24 +0530

    Add files via upload

---
+++ if-else3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+//a>b
+int main()
+{
+    int a,b;
+
+    cin>>a;
+    cin>>b;
+    
+    if(a>b)
+    {
+        cout<<"Yes A is Greter ";
+    }
+    else
+    {
+        cout<<"No B is Greater ";
+    }
+}
