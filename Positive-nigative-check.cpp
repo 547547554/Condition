ec13935ced9eae0fac096ff0db9a525dc44561dc
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-09-14 23:06:40 +0530

    Add files via upload

---
+++ Positive-nigative-check.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a;
+    cout << "Enter your a Number : ";
+    cin >> a;
+
+    if (a>0)
+    {
+        cout<<"Positive Number";
+    }
+
+    else if (a==0)
+    {
+          cout<<"Your  Number is 0 ";
+    }
+    else
+    {
+         cout<<"Nigative  Number";
+    }
+}
