49a1098ac4ab87087b54ebd44f765fe6c9bdf97d
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-09-16 23:03:53 +0530

    Add files via upload

---
+++ vovel-or-consonent.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char latter = 'b';
+   
+    if(latter=='a')
+    {
+        cout<<latter<<" is vovel " ;
+    }
+
+    else if(latter=='e')
+    {
+        cout<<latter<<" is vovel " ;
+    }
+
+    else if(latter=='i')
+    {
+        cout<<latter<<" is vovel " ;
+    }
+
+    else if(latter=='0')
+    {
+        cout<<latter<<" is vovel " ;
+    }
+
+    else if(latter=='u')
+    {
+        cout<<latter<<" is vovel " ;
+    }
+    else
+    {
+        cout<<"Consonent";
+    }
+
+}
